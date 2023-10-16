f1f5daec7e5ebd5bfeb9fc5ca3179225251f9fa5
cds05197 <cds05197@naver.com>
2023-10-16 14:10:48 +0900

    Test CI/CD

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def HelloWorld():
 	return {"hello":"world!"}
 
+@app.route('/test')
+def Hellotest():
+	return {"test":"success!"}
+
 if __name__ == "__main__":
 	app.run(debug=True, host="0.0.0.0", port=5000)
